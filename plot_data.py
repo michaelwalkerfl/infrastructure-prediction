fec5142385790e4e20465b363b905792b722567c
Michael Walker <michael@verus.io>
2016-04-25 23:09:16 -0400

    Added data chart.

---
+++ plot_data.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+from sys import argv
+
+data = sp.genfromtxt("data/argv[0].tsv", delimiter="\t")
+
+def plot(data):
+	plt.scatter(x,y)
+	plt.title("Web traffic over the last month")
+	plt.xlabel("Time")
+	plt.ylabel("Hits/hour")
+	plt.xticks([w*7*24 for w in range(10)],
+	 ['week %i'%w for w in range(10)])
+	plt.autoscale(tight=True)
+	plt.grid()
+	plt.show()

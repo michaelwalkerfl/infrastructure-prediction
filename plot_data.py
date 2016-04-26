c16ce1dd7dd106cee71c80a0308611dc22b84d22
Michael Walker <michael@verus.io>
2016-04-26 14:48:44 -0400

    Fixed chart and file argument.

--- plot_data.py
+++ plot_data.py
@@ -4,21 +4,27 @@ import matplotlib.pyplot as plt
 import scipy as sp
 from sys import argv
 
-data = sp.genfromtxt("data/argv[0].tsv", delimiter="\t")
+data = sp.genfromtxt("data/%s" % argv[1], delimiter="\t")
+
+x = data[:,0]
+y = data[:,1]
+
+x = x[~sp.isnan(y)]
+y = y[~sp.isnan(y)]
 
 def plot(data):
-	plt.scatter(x,y)
-	plt.title("Web traffic over the last month")
-	plt.xlabel("Time")
-	plt.ylabel("Hits/hour")
-	plt.xticks([w*30*24 for w in range(10)],
-	 ['week %i'%w for w in range(10)])
-	plt.autoscale(tight=True)
-	plt.grid()
-	plt.show()
+    plt.scatter(x,y)
+    plt.title("Web traffic over the last month")
+    plt.xlabel("Time")
+    plt.ylabel("Hits/hour")
+    plt.xticks([w*30*24 for w in range(10)],
+     ['week %i'%w for w in range(10)])
+    plt.autoscale(tight=True)
+    plt.grid()
+    plt.show()
 
 
 if __name__ == '__main__':
-	main()
+	plot(data)
 
 
